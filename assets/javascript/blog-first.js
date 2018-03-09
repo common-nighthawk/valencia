9b4013d30a86a5dc1e8bd42b9c847f97dcbebaff
Daniel <danielallendeutsch@gmail.com>
2018-03-08 21:05:24 -0800

    Fixups post launch.

--- assets/javascript/blog-first.js
+++ assets/javascript/blog-first.js
@@ -6,8 +6,8 @@ $(document).ready(function() {
 
     $.getJSON("https://www.googleapis.com/blogger/v3/blogs/651190750087447841/posts/" + postId + "/?key=AIzaSyC2KvolQ8HF1xQfD0hyNnkvWPHJaDeZAwA", function(data) {
       var content = data["content"].replace(/<a href.*img.*<\/a>/g, "").replace(/<br \/>/g, "");
-      if (content.length > 400) {
-        content = content.substring(0, 400);
+      if (content.length > 235) {
+        content = content.substring(0, 235);
         content = content + "..."
       }
       $(".featured-news").html(content + " <a class='fancy-link' href='post.html?" + postId + "'>read more</a>");
